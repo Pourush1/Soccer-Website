edc3f38f1f2a5fa7e4dc1b3e56f2766a07dd4395
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-04-13 19:28:33 -0400

    Write Validation handler logic for registration, since validator only checks for string needed to do some extra things for validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User model
 const User = require("../../models/User");
 
@@ -19,8 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register route
 // @access  Public
 router.post("/register", (req, res) => {
+  //Destructuring concept used here
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
+      errors.email = "Email already exists";
       return res.status(400).json({ email: "Email already exist" });
     } else {
       const avatar = gravatar.url(req.body.email, {
