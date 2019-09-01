05b8daa5979bb51be7f11699e552528ab3f415a2
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-09-01 13:38:11 -0500

    Tested the RapidAPI to get the data of the leagues.Incorporated that in the code

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,52 @@ import SoccerPage from "./components/layout/SoccerPage";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={SoccerPage} />
-        <div className="container">
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      leagues: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://sportsop-soccer-sports-open-data-v1.p.rapidapi.com/v1/leagues",
+      {
+        method: "get",
+        headers: {
+          "x-rapidapi-key":
+            "099c34f6acmsh07c750b01c740bap1cf4a2jsnd87a2f2e3082",
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data.data.leagues);
+        this.setState({ leagues: data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      //Wrap around Router to make router available to all other components
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={SoccerPage} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
